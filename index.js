6c25011d351be09670cf6d24f8ad5f215e27eb20
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-08-24 13:07:24 +0200

    add true script manager

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const http = require('http')
+const manager = require('script-manager')()
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
+  var data = ''
+  req.on('data', function (chunk) {
+    data += chunk.toString()
+  })
+
+  req.on('end', function () {
+    manager.ensureStarted((err) => {
+      if (err) {
+        res.statusCode = 500
+        res.setHeader('Content-Type', 'text/plain')
+        return res.end('Error when starting script manager ' + err.stack)
+      }
+
+      const body = JSON.parse(data)
+
+      manager.execute(body.inputs, body.options, (err, scriptResponse) => {
+        if (err) {
+          res.statusCode = 500
+          res.setHeader('Content-Type', 'text/plain')
+          return res.end('Error when executing script ' + err.stack)
+        }
+
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        return res.end(JSON.stringify(scriptResponse))
+      })
+    })
+  })
 })
 
 server.listen(3000)
